9fc67211c57f616671c2d62af291b5a2b47801d1
PabloJ09 <pablohera04@gmail.com>
2024-09-28 13:55:31 -0600

    conexion base de datos, modelos y DbContext

---
+++ Models/Factura.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Taller_y_Repuestos.Models;
+
+public partial class Factura
+{
+    public int FacturaId { get; set; }
+
+    public int ClienteId { get; set; }
+
+    public DateTime Fecha { get; set; }
+
+    public decimal Total { get; set; }
+
+    public decimal? Impuestos { get; set; }
+
+    public decimal? Descuento { get; set; }
+
+    public virtual Cliente Cliente { get; set; } = null!;
+
+    public virtual ICollection<DetallesFactura> DetallesFacturas { get; set; } = new List<DetallesFactura>();
+}
