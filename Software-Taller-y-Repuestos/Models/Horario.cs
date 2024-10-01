b418bcbe97bc9ffe33af73056d2dd1d0fbba3b46
PabloJ09 <pablohera04@gmail.com>
2024-09-30 22:59:52 -0600

    Conexion, DbContext y modelos

---
+++ Software-Taller-y-Repuestos/Models/Horario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Software_Taller_y_Repuestos.Models;
+
+public partial class Horario
+{
+    public int HorarioId { get; set; }
+
+    public int UsuarioId { get; set; }
+
+    public DateTime Fecha { get; set; }
+
+    public decimal HorasTrabajadas { get; set; }
+
+    public decimal HorasExtras { get; set; }
+
+    public decimal Ausencias { get; set; }
+
+    public decimal Permisos { get; set; }
+
+    public virtual Usuario Usuario { get; set; } = null!;
+}
---
+++ Software-Taller-y-Repuestos/Models/Role.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Software_Taller_y_Repuestos.Models;
+
+public partial class Role
+{
+    public int RolId { get; set; }
+
+    public string NombreRol { get; set; } = null!;
+
+    public virtual ICollection<Usuario> Usuarios { get; set; } = new List<Usuario>();
+}
