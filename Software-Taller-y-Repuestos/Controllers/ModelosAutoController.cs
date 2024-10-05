00cf20810d77f7ef6c92b61b504b5c75ca628ce4
PabloJ09 <pablohera04@gmail.com>
2024-10-04 20:15:02 -0600

    - Actualizacion BBDD (Producto, Proveedore, DbContext) - Controladores para la Gestion de Inventario (Producto, Categoria, ModelosAuto, Provedoree)

---
+++ Software-Taller-y-Repuestos/Controllers/ModelosAutoController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Software_Taller_y_Repuestos.Controllers
+{
+    public class ModelosAutoController : Controller
+    {
+        // GET: ModelosAutoController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: ModelosAutoController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ModelosAutoController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ModelosAutoController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ModelosAutoController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ModelosAutoController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ModelosAutoController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ModelosAutoController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Software-Taller-y-Repuestos/Models/Proveedore.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Software_Taller_y_Repuestos.Models;
+
+public partial class Proveedore
+{
+    public int ProveedorId { get; set; }
+
+    public string Nombre { get; set; } = null!;
+
+    public string Direccion { get; set; } = null!;
+
+    public string Telefono { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string? Ruc { get; set; }
+
+    public virtual ICollection<Producto> Productos { get; set; } = new List<Producto>();
+}
