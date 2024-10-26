15edd1e58ff7823f2135e297963973b22f7dd88b
DESKTOP-RSANVHA\Fabiola Mora <fabiola9795@gmail.com>
2024-10-25 18:52:02 -0600

    interfaz para parte de factura
    se crea interfaz opcional para facturacion

---
+++ Software-Taller-y-Repuestos/Controllers/FacturaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Software_Taller_y_Repuestos.Controllers
+{
+    public class FacturaController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
